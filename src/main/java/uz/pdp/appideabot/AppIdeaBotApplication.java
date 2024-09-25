20e0e90b22f5e57715065b75345a2f74ad568222
Abdulaziz <wwxeww1wwxeww@gmail.com>
2024-09-25 16:01:03 +0500

    created

---
+++ src/main/java/uz/pdp/appideabot/AppIdeaBotApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.appideabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppIdeaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppIdeaBotApplication.class, args);
+    }
+
+}
